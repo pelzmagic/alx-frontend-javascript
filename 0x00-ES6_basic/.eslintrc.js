99d1a962580c9a80d87ecfc274018a8ab076e28e
pelzmagic <akinloyepelz@gmail.com>
2022-12-22 21:12:26 +0000

    const or let

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,21 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+	  'airbnb-base',
+	  'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+   ],
+ },
+ overrides:[
+   {
+     files: ['*.js'],
+     excludedFiles: 'babel.config.js',
+   }
+ ]
+};
+
+	  	
