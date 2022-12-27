c813f3f649388b38bd42187dbed97c25970d330c
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-12-27 00:20:21 -0800

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,12 @@
-bdbdhjdjehe
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') return this._size;
+    if (hint === 'string') return this._location;
+    return this;
+  }
+}
