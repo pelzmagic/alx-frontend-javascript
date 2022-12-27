ee412227bae8925c3ef35c8feaac20737343e0b3
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-12-27 00:19:19 -0800

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,10 @@
-hdggdhdhdj
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
